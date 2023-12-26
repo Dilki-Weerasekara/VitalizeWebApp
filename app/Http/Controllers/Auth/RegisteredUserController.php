3b1c13fa2ddf5692da1c82d2a5d68360fd907648
Dilki Weerasekara <dilki.hashi@gmail.com>
2023-12-26 16:45:35 +0000

    Complete Register Page Authentication

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
 use Illuminate\View\View;
+use Str;
 
 class RegisteredUserController extends Controller
 {
@@ -31,16 +32,39 @@ class RegisteredUserController extends Controller
     public function store(Request $request): RedirectResponse
     {
         $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'profileType' => 'required',
+            'profession' => 'required|string|max:255',
+            'username' => 'required|string|max:255',
+            'email' => 'sometimes|string|email|max:255|unique:' . User::class,
+            'tel' => 'sometimes|string|max:255|unique:users,mobile',
+            'profile' => 'required|image|mimes:png,jpg,jpeg|max:1024',
+            'gender' => 'required',
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
+        //create a new object from user
         $user = User::create([
-            'name' => $request->name,
+            'uuid'=>Str::uuid(),
+            'first_name' => $request->first_name, //database table column name = UI name
+            'last_name' => $request->last_name,
+            'profile_type'=>  $request-> profileType,
+            'profession'=>  $request-> profession,
+            'username' => $request->username,
+            'gender' => $request->gender,
+            'profile' => $profile ?? "",
             'email' => $request->email,
+            'mobile' => $request->tel,
             'password' => Hash::make($request->password),
         ]);
+
+        //store the profile file image
+        $profile = "";
+        if ($request->file("profile")) {
+            $profile = $request->file("profile")->store("profiles","public");
+        }
+
 
         event(new Registered($user));
 
