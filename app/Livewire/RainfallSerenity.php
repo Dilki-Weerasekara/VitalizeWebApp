697b974e356fae5eb95f23b0b315d3ac6e891570
Dilki Weerasekara <dilki.hashi@gmail.com>
2024-01-03 23:10:27 +0000

    Add rainfall video selection page

---
+++ app/Livewire/RainfallSerenity.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class RainfallSerenity extends Component
+{
+    public function render()
+    {
+        return view('livewire.rainfall-serenity')->extends("layouts.app");
+    }
+}
